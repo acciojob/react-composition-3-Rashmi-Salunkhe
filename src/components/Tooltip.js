1a02d74280bf2ef2d3068dde5a6625856f324740
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-31 11:12:35 +0530

    Create Tooltip.js

---
+++ src/components/Tooltip.js
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react';
+import '../styles/Tooltip.css'
+const Tooltip =({ text, children})=>{
+  const [visible, setVisible] =useState(false);
+  const showTooltip =()=>setVisible(true);
+  const hideTooltip =()=>setVisible(false);
+  return (
+    <div  onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
+        {children}
+        {visible && <div className="tooltiptext">{text}</div>}
+    </div>
+  );
+}
+export default Tooltip;
